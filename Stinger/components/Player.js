8bca2c3c593d3de9f1ed58b468ee3352f0ad57b8
Andrei <andreidei0406@yahoo.com>
2023-06-01 11:24:56 +0300

    finished player component, base project done, to do: implement multiple features and refactor code

--- Stinger/components/Player.js
+++ Stinger/components/Player.js
@@ -1,9 +1,20 @@
 import { currentTrackIdState, isPlayingState } from "@/atoms/songAtom";
 import useSongInfo from "@/hooks/useSongInfo";
 import useSpotify from "@/hooks/useSpotify";
+import {
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  RewindIcon,
+  SwitchHorizontalIcon,
+  VolumeUpIcon,
+} from "@heroicons/react/solid";
+import { VolumeUpIcon as VolumeDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
+import { debounce } from "lodash";
 
 function Player() {
   const spotifyApi = useSpotify();
@@ -29,6 +40,18 @@ function Player() {
     }
   };
 
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false);
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true);
+      }
+    });
+  };
+
   useEffect(() => {
     if (spotifyApi.getAccessToken() && !currentTrackId) {
       //fetch song info
@@ -37,14 +60,71 @@ function Player() {
     }
   }, [currentTrackId, spotifyApi, session]);
 
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]);
+
+  const debouncedAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => {});
+    }, 500),
+    []
+  );
+
   return (
-    <div>
+    <div
+      className="h-24 bg-gradient-to-b from-black to-gray-900 text-white
+    grid grid-cols-3 text-cs md:text-base px-2 md:px-8"
+    >
       {/* left */}
-      <div>
+      <div className="flex items-center space-x-4">
         <img
           className="hidden md:inline h-10 w-10"
           src={songInfo?.album.images?.[0]?.url}
           alt=""
+        />
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0].name}</p>
+        </div>
+      </div>
+      {/* center */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon
+          className="button"
+          onClick={() => spotifyApi.skipToPrevious()}
+        />
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        )}
+        <FastForwardIcon
+          className="button"
+          onClick={() => spotifyApi.skipToNext()}
+        />
+        <ReplyIcon className="button" />
+      </div>
+      {/* right */}
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
+        <input
+          className="w-14 md:w-28"
+          type="range"
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
+          min={0}
+          max={100}
+        />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
         />
       </div>
     </div>
--- Stinger/components/Sidebar.js
+++ Stinger/components/Sidebar.js
@@ -2,13 +2,13 @@ import { playlistIdState } from "@/atoms/playlistAtoms";
 import useSpotify from "@/hooks/useSpotify";
 import {
   HomeIcon,
-  SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
-  HeartIcon,
   RssIcon,
+  SearchIcon,
 } from "@heroicons/react/outline";
-import { signOut, useSession } from "next-auth/react";
+import { HeartIcon } from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 
@@ -30,7 +30,7 @@ function Sidebar() {
     <div
       className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 
     overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem]
-    hidden md:inline-flex"
+    hidden md:inline-flex pb-36"
     >
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
@@ -50,12 +50,12 @@ function Sidebar() {
           <PlusCircleIcon className="h-5 w-5" />
           <p>Create Playelist</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
+        <button className="flex items-center space-x-2 text-blue-500 hover:text-white">
           <HeartIcon className="h-5 w-5" />
           <p>Liked songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
+          <RssIcon className="h-5 w-5 text-green-500" />
           <p>Your episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
